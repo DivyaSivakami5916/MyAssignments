04492b59a63ce8f5a4723cfe638f420fdbbc0791
DivyaSivakami5916 <139773477+DivyaSivakami5916@users.noreply.github.com>
2023-07-17 18:18:08 +0530

    Add files via upload
    Day 1- Home Assignments Completed.

---
+++ day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+	
+		float version=100.2f;
+		String name="firefox";
+		Boolean visible=true;
+		int year=1998;
+		char logo='m';
+		System.out.println("Firefox Version is: "+ version);
+		System.out.println("Browser name is: "+ name);
+		System.out.println("Browser visibility is: "+ visible);
+		System.out.println("Browser Start Year is: "+ year);
+		System.out.println("Browser logo is: "+ logo);
+	}
+
+}
---
+++ day1/car.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class car {
+
+	public static void main(String[] args) {
+		int price=100000;        
+        String brandName="Tata";     
+        char logo='T';        
+        float fuel=12.45f;    
+        String fuelCapacity="12.65lt";
+        double modelVariant=456.2343565757;
+        boolean isNew=true;        
+        long phNo=673528208366722l;
+        System.out.println("logo is : "+logo);        
+        System.out.println(logo);        
+        System.out.println("The brand name of the Car :"+brandName);
+        System.out.println(price);
+        System.out.println(fuel);
+        System.out.println(modelVariant +": "+isNew);
+        System.out.println(phNo);
+        System.out.println(fuelCapacity);
+	}
+
+}
