d5ec1428ff46fd0a83a5ae050e567a207310eeac
DivyaSivakami5916 <139773477+DivyaSivakami5916@users.noreply.github.com>
2023-07-25 21:52:52 +0530

    FindAccount.java

--- week2/day1/FindAccount.java
+++ week2/day1/FindAccount.java
@@ -18,9 +18,9 @@ public class FindAccount {
 		driver.findElement(By.className("decorativeSubmit")).click();
 		driver.findElement(By.linkText("CRM/SFA")).click();
 		driver.findElement(By.linkText("Accounts")).click();
-		driver.findElement(By.xpath("//a[text()='Find Accouts']")).click();
+		driver.findElement(By.xpath("//a[text()='Find Accounts']")).click();
 		driver.findElement(By.xpath("(//input[@name='accountName'])[2]")).sendKeys("Credit Limited Account");
-		driver.findElement(By.xpath("(//button[text()='Find Accouts']")).click();
+		driver.findElement(By.xpath("(//button[text()='Find Accounts']")).click();
 		driver.findElement(By.linkText("Credit Limited Account")).click();
 		driver.findElement(By.linkText("Edit")).click();
 		String e = driver.findElement(By.className("(//span[@class='tabletext'])[3]")).getText();
