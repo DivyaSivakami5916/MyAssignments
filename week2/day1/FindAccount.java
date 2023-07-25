ba4613a18ad2d61c3a98bbbf6e6b05887a571b3f
DivyaSivakami5916 <139773477+DivyaSivakami5916@users.noreply.github.com>
2023-07-25 21:08:24 +0530

    Add files via upload
    week2.day1

---
+++ week2/day1/FindAccount.java
@@ -0,0 +1,35 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import java.time.Duration;
+public class FindAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		WebElement uname=driver.findElement(By.id("username"));
+		uname.sendKeys("demoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Accounts")).click();
+		driver.findElement(By.xpath("//a[text()='Find Accouts']")).click();
+		driver.findElement(By.xpath("(//input[@name='accountName'])[2]")).sendKeys("Credit Limited Account");
+		driver.findElement(By.xpath("(//button[text()='Fid Accouts']")).click();
+		driver.findElement(By.linkText("Credit Limited Account")).click();
+		driver.findElement(By.linkText("Edit")).click();
+		String e = driver.findElement(By.className("(//span[@class='tabletext'])[3]")).getText();
+		System.out.println(e);
+		String d = driver.findElement(By.className("(//td[@class='tabletext'])[1]")).getText();
+		System.out.println(d);
+		String title=driver.getTitle();
+		System.out.println(title);
+		driver.close();
+	}
+
+}
