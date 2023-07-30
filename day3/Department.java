2f43ead768994aa8df1df85ecf839cc93a841d9b
DivyaSivakami5916 <139773477+DivyaSivakami5916@users.noreply.github.com>
2023-07-30 23:07:07 +0530

    Delete Department.java

--- day3/Department.java
+++
@@ -1,9 +0,0 @@
-package week2.day3;
-
-public class Department extends College{
-	
-	public void DepartmentName()
-	{
-		System.out.println("The Department Name is : Computer Science and Engineering");
-	}
-}
