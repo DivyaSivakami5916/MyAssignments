d4a0a2c26e59f9fd158392a71121dc0ec3ecd27b
DivyaSivakami5916 <139773477+DivyaSivakami5916@users.noreply.github.com>
2023-07-30 23:13:52 +0530

    Add files via upload

---
+++ Week2day3/day3/Computer.java
@@ -0,0 +1,9 @@
+package week2.day3;
+
+public class Computer {
+
+	public void ComputerModels()
+	{
+		System.out.println("Computer Model is Apple IIGS");
+}
+}
---
+++ Week2day3/day3/Department.java
@@ -0,0 +1,9 @@
+package week2.day3;
+
+public class Department extends College{
+	
+	public void DepartmentName()
+	{
+		System.out.println("The Department Name is : Computer Science and Engineering");
+	}
+}
